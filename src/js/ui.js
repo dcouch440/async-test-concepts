0ac35177303beb746cd297e7e42224d3905f1f85
dcouch440 <dcouch440@gmail.com>
2021-02-09 23:08:24 -0800

    add more descriptive names to object keys

--- src/js/ui.js
+++ src/js/ui.js
@@ -4,9 +4,9 @@ export const search = (thisArg) => {
     const {value} = document.getElementById('search-input');
     const weatherType = await WeatherService.receive(value);
     return {
-      async_weatherGif: GifService.receive(weatherType.feel),
-      weatherFeelsLike: weatherType.feel,
-      weatherFeelsTemp: weatherType.temp
+      async_gif: GifService.receive(weatherType.feel),
+      sync_feel: weatherType.feel,
+      sync_temp: weatherType.temp
     };
   };
   document.getElementById("giphy-search").addEventListener(
@@ -15,8 +15,8 @@ export const search = (thisArg) => {
       const textElement = document.getElementById('sky');
       (async () => {
         const api = await callApis();
-        gifElement.innerHTML = `<img src=${await api.async_weatherGif}>`;
-        textElement.innerHTML = `Feels like: ${api.weatherFeelsLike}, Temp: ${api.weatherFeelsTemp}`;
+        gifElement.innerHTML = `<img src=${await api.async_gif}>`;
+        textElement.innerHTML = `Feels like: ${api.sync_feel}, Temp: ${api.sync_temp}`;
       })();
     });
 };
